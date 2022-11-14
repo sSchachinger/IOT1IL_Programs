cd722f5d76973199c5f07496d5f24b5488689b8f
Simon Schachinger <s.schachinger@gmail.com>
2022-11-14 20:14:18 +0100

    Projektdateien hinzufügen.

---
+++ TCP.Client/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP.Client
+{
+    class Program
+    {
+        static TcpClient client;
+        static NetworkStream stream;
+        static bool gRunVar = false;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "TCP-Client";
+            stream = initConnection();
+
+            Task tSend = new Task(sendTask);
+            Task tRecieve = new Task(recieveTask);
+            tSend.Start();
+            tRecieve.Start();
+
+            while (!gRunVar)
+            {
+                Thread.Sleep(10000);
+            }
+        }
+        static void recieveTask()
+        {
+            var reader = new StreamReader(stream);
+            string request = "";
+            bool runVar = false;
+
+            do
+            {
+                try
+                {
+                    request = reader.ReadLine();
+                }
+                catch (Exception)
+                {
+                }
+                finally { reader.Close(); }
+
+                if (request == "Exit")
+                {
+                    runVar = true;
+                    client.Close();
+                    Console.WriteLine("[Server] closed");
+                }
+                else if (request == null)
+                { }
+                else if (request.Contains("Equation"))
+                    Console.WriteLine("[Server]:" + request);
+                else if (request.Contains("Stringcheck"))
+                    Console.WriteLine("[Server]:" + request);
+
+
+
+                Thread.Sleep(100);
+            } while (!runVar);
+        }
+
+        static void sendTask()
+        {
+            bool runVar = false;
+            do
+            {
+                Console.WriteLine("[Client] What to do? Equation, Exit or Stringcheck");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case ("Equation"):
+                        sendEquation();
+                        break;
+                    case ("Stringcheck"):
+                        sendStringcheck();
+                        break;
+                    case ("Exit"):
+                        runVar = true;
+                        gRunVar = true;
+                        sendExit();
+                        client.Close();
+                        Console.WriteLine("[Client] closed");
+                        break;
+                    default:
+                        break;
+                }
+                Thread.Sleep(100);
+            } while (!runVar);
+        }
+
+        static NetworkStream initConnection()
+        {
+            string hostname = "127.0.0.1";
+            var port = 3001;
+            client = new TcpClient(hostname, port);
+            return client.GetStream();
+        }
+
+        static void sendEquation()
+        {
+            var reader = new StreamReader(stream, Encoding.ASCII);
+            var writer = new StreamWriter(stream, Encoding.ASCII);
+
+            Console.WriteLine("[Client] Insert Equation:");
+            string input = Console.ReadLine();
+
+            writer.WriteLine($"Equation:{input}");
+            writer.Flush();
+
+        }
+        static void sendStringcheck()
+        {
+
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+
+            Console.WriteLine("[Client] Insert two Strings separated by ':' :");
+            string input = Console.ReadLine();
+
+            writer.WriteLine($"Stringcheck:{input}");
+            writer.Flush();
+        }
+        static void sendExit()
+        {
+            var reader = new StreamReader(stream, Encoding.UTF8);
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+
+            writer.WriteLine($"Exit");
+            writer.Flush();
+        }
+    }
+}
---
+++ TCP.Server/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
+
+namespace TCP.Server
+{
+    class Program
+    {
+        static NetworkStream stream;
+        static TcpClient client;
+        static bool gRunVar = false;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "TCP-Server";
+            stream = initConnection();
+            Task tRecieve = new Task(recieveTask);
+            tRecieve.Start();
+
+            while (!gRunVar)
+            {
+                Thread.Sleep(10000);
+            }
+        }
+
+        static void recieveTask()
+        {
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+
+            string request;
+            bool runVar = false;
+
+            do
+            {
+                request = reader.ReadLine();
+
+                if (request == "Exit")
+                {
+                    gRunVar = true;
+                    runVar = true;
+                    client.Close();
+                    Console.WriteLine("[Server] closed");
+                }
+                else if (request.Contains("Equation"))
+                    sendResponse(Equation(request), writer);
+                else if (request.Contains("Stringcheck"))
+                    sendResponse(Stringcheck(request), writer);
+                Thread.Sleep(100);
+            } while (!runVar);
+        }
+
+        static NetworkStream initConnection()
+        {
+            var adress = IPAddress.Parse("127.0.0.1");
+            var port = 3001;
+            var listener = new TcpListener(adress, port);
+
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            return client.GetStream();
+        }
+
+        static void sendResponse(string response, StreamWriter writer)
+        {
+            writer.WriteLine($" Response: {response}");
+            writer.Flush();
+        }
+
+        static string Equation(string request)
+        {
+            char[] operators = { '*', '/', '+', '-' };
+            char separatedChar = ' ';
+            int[] numbers = new int[2];
+
+            foreach (var item in operators)
+            {
+                if (request.Contains(item))
+                {
+                    string[] strings = request.Split(item);
+                    strings[0] = strings[0].Remove(0, 9);
+                    for (int i = 0; i < strings.Length; i++)
+                    {
+                        if (!int.TryParse(strings[i], out numbers[i]))
+                            Console.WriteLine("Not a number");
+                    }
+                    separatedChar = item;
+                }
+            }
+            double result = 0;
+            switch (separatedChar)
+            {
+                case ('*'):
+                    result = numbers[0] * numbers[1];
+                    break;
+                case ('/'):
+                    if (numbers[1] == 0)
+                        return "Division trough 0";
+                    result = numbers[0] / numbers[1];
+                    break;
+                case ('+'):
+                    result = numbers[0] + numbers[1];
+                    Console.WriteLine($"[Server]:{result}");
+                    break;
+                case ('-'):
+                    result = numbers[0] - numbers[1];
+                    break;
+            }
+            return "Equation: " + result.ToString();
+        }
+
+        static string Stringcheck(string request)
+        {
+            string[] strings = request.Split(':');
+            if (strings[1].Equals(strings[2]))
+            {
+                Console.WriteLine("[Server]: Equal string");
+                return "Stringcheck: Equal String";
+            }
+            else
+            {
+                Console.WriteLine("[Server]: Not equal string");
+                return "Stringcheck: Not equal String";
+            }
+        }
+    }
+}
+
